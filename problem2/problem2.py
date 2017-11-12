56149c81e5692f3115d20bbbd70d93786c34ef33
jj <jjy@somewhere.com>
2017-11-12 22:12:37 +0900

    solved Second Problem

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -10,5 +10,16 @@ def fibonacci(num):
     return fibo
 
 
-def findEven():
-    pass
+def evenFibonacciSum():
+    fibo = []
+    evenSum = 0
+    i = 2
+    fibo.append(1)
+    fibo.append(2)
+    evenSum = 2
+    while fibo[i-1] < 4000000:
+        fibo.append(fibo[i - 1] + fibo[i - 2])
+        if fibo[i] % 2 == 0:
+            evenSum += fibo[i]
+        i += 1
+    return evenSum
