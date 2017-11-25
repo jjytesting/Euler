56c987eb1cc3e66d5a228f788edde8462967feed
jj <jjy@somewhere.com>
2017-11-26 01:43:00 +0900

    real generator

--- problem2/evenFibonacci.py
+++ problem2/evenFibonacci.py
@@ -53,15 +53,42 @@ def fibonacciBeforeMaxWithoutRepeat(maxNum):
 
 print(evenSumFibonacci(fibonacciBeforeMax(4000000)))
 
-def fibonacciGenerator(index):
+
+def fibonacciListComprehension(index):
     fibo = []
     if index == 0:
-        fibo.append(1)
+        fibo.append(0)
     elif index == 1:
-        fibo = fibonacciGenerator(0)
-        fibo.append(2)
+        fibo = fibonacciListComprehension(0)
+        fibo.append(1)
     elif index > 1:
-        fibo =  fibonacciGenerator(1) + [fibonacciGenerator(x-1)[-1] + fibonacciGenerator(x-2)[-1] for x in range(2,index+1)]
+        fibo =  fibonacciListComprehension(1) + [fibonacciListComprehension(x - 1)[- 1] + 
+                fibonacciListComprehension(x - 2)[- 1] for x in range(2, index + 1)]
     return fibo
 
-print(fibonacciGenerator(10))
+
+print(fibonacciListComprehension(10))
+
+
+def fibonacciGenerator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a+b
+
+
+def makeFibo(maxNum):
+    itr = fibonacciGenerator()
+    fibo = []
+    for i in itr:
+        if i > maxNum:
+            break
+        else:
+            fibo.append(i)
+    return fibo
+
+print(evenSumFibonacci(makeFibo(4000000)))
+
+
+
+
